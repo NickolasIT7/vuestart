21ad20b7b51f446af7b496e4c418fd2fd5e8c6b5
NickolasIT7 <justdoitagainnick@gmail.com>
2024-02-23 19:03:18 +0300

    +ToDo

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,45 @@ import CounterButton from './components/CounterButton.vue'
 import OptionsApi from './components/OptionsApi.vue'
 import LinksComponent from './components/LinksComponent.vue'
 import YesNoWTF from './components/YesNoWTF.vue'
+import ToDo from './components/ToDo.vue'
 import {ref} from 'vue'
 const hide = ref(false)
+const isActive = ref(false)
+const activeClass = 'active'
+const uppercase = 'uppercase'
+const fontSize = 16
+const items = ref([
+  { message: 'Foo' },
+  { message: 'Bar' }
+])
+let i = 0 
+const addElem = ()=>{
+  items.value.push({message:'text'+i})
+  i++
+}
+const removeElemEnd = ()=>{
+  items.value.pop()
+}
+const removeElemStart = ()=>{
+  items.value.shift()
+}
+
 </script>
 
 <template>
+  <ToDo></ToDo>
   <header>
     <LinksComponent></LinksComponent>
     <YesNoWTF></YesNoWTF>
-  </header>  
+  </header>
+  <div>
+    <button @click="addElem">Добавить</button>
+    <button @click="removeElemEnd">Удалить с конца</button>
+    <button @click="removeElemStart">Удалить с начала</button>
+    <template v-for="(value, index) of items" >
+      <p>{{ value }} {{ index }}</p>
+    </template>
+  </div>  
   <CounterButton v-bind:start="10"/>
   <CounterButton :start="15"/>
   <CounterButton style="background:yellow"/>
@@ -20,15 +50,42 @@ const hide = ref(false)
   <CounterButton/>
   <CounterButton/>
   <CounterButton/>
-  <TemplateButton>Content from parent</TemplateButton>
+  <TemplateButton><span :class="[uppercase, isActive ? activeClass : '']">Content from parent</span></TemplateButton>
   <TemplateButton></TemplateButton>
   <br>
-  <OptionsApi v-if="!hide" v-on:hide="hide=true"/>
-  <button @click="hide=false">Show hidden Button</button>
+  <template v-if="!hide">
+    <OptionsApi v-on:hide="hide=true"/>
+    <p :style="{ color: 'red', fontSize: fontSize + 'px' }">Эта кнопка и текст исчезнут, когда счётчик достигнет 5</p>
+  </template>
+  <button class="uppercase" :class="{ active: isActive, isHidden:hide }" @click="hide=false">Show hidden Button</button>
+  <button :class="isActive?'':'nonActive'" @click="isActive = !isActive">Toggle</button>
+  <div v-show="!hide">
+    <div v-if="Math.random() > 0.7">
+      Сейчас меня видно
+    </div>
+    <div v-else-if="Math.random() < 0.3">
+      А теперь — нет 03
+    </div>
+    <div v-else>
+      А теперь — нет else
+    </div>
+  </div>
 </template>
 
 <style scoped>
   .myButton {
     background:red;
+  }
+  .active {
+    background: green;
+  }
+  .nonActive {
+    background: orange;
+  }
+  .uppercase {
+    text-transform: uppercase;
+  }
+  .isHidden {
+    padding: 50px;
   }
 </style>
--- src/components/YesNoWTF.vue
+++ src/components/YesNoWTF.vue
@@ -1,36 +1,38 @@
 <template>
+  <div style="margin: 20px auto; max-width: 400px;">
     <input type="text" v-model="question">
     <div>{{ russianAnswer }}</div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    data() {
-      return {
-        question: '',
-        answer: 'Вопросы обычно заканчиваются вопросительным знаком. ;-)'
-      }
-    },
-    watch: {
-      // при каждом изменении `question` эта функция будет запускаться
-      question(newQuestion, oldQuestion) {
-        if (newQuestion.indexOf('?') > -1) {
-          this.getAnswer()
-        }
-      }
-    },
-    methods: {
-      async getAnswer() {
-        this.answer = 'Думаю...'
-        const resp = await fetch('https://yesno.wtf/api')
-        const data = await resp.json()
-        this.answer = data.answer
-      }   
-    },
-    computed: {
-      russianAnswer() {
-        return this.answer == 'yes'? 'Да': this.answer == 'no' ? 'Нет' : this.answer
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      question: '',
+      answer: 'Вопросы обычно заканчиваются вопросительным знаком. ;-)'
+    }
+  },
+  watch: {
+    // при каждом изменении `question` эта функция будет запускаться
+    question(newQuestion, oldQuestion) {
+      if (newQuestion.indexOf('?') > -1) {
+        this.getAnswer()
       }
     }
+  },
+  methods: {
+    async getAnswer() {
+      this.answer = 'Думаю...'
+      const resp = await fetch('https://yesno.wtf/api')
+      const data = await resp.json()
+      this.answer = data.answer
+    }   
+  },
+  computed: {
+    russianAnswer() {
+      return this.answer == 'yes'? 'Да': this.answer == 'no' ? 'Нет' : this.answer
+    }
   }
-  </script>
+}
+</script>
